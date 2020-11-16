ec10e78ee341deb5fd6f8a4e6483ef572ef978e4
sm492am <stanislav.macak@student.tuke.sk>
2020-11-16 09:26:17 +0100

    unet dice loss

--- Unetmodel.py
+++ Unetmodel.py
@@ -73,3 +73,18 @@ def model(img_shape):
 
     outputs = layers.Conv2D(1, (1, 1), activation='sigmoid')(decoder0)
     return models.Model(inputs=[inputs], outputs=[outputs])
+
+def bce_dice_loss(y_true, y_pred):
+    loss = losses.binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+    return loss
+def dice_coeff(y_true, y_pred):
+    smooth = 1.
+    # Flatten
+    y_true_f = tf.reshape(y_true, [-1])
+    y_pred_f = tf.reshape(y_pred, [-1])
+    intersection = tf.reduce_sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth)
+    return score
+def dice_loss(y_true, y_pred):
+    loss = 1 - dice_coeff(y_true, y_pred)
+    return loss
